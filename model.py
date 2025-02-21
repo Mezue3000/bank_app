ad06e3c5a7e539bc22cab426347070e33eb6f46e
mezue30000 <mezue3000>
2025-02-21 15:02:51 +0100

    created transaction model/relationship

--- model.py
+++ model.py
@@ -41,8 +41,36 @@ class Account(SQLModel, table=True):
                         foreign_key="customer.customer_id", sa_column_kwargs={"onupdate": "CASCADE", "ondelete": "NO ACTION"})
     
     customer:Customer = Relationship(back_populates="accounts")
+    transactions:List[Transaction] = Relationship(back_populates="account") 
      
     __table_args__ = (
         CheckConstraint("account_type IN('savings', 'current', 'fixed', 'domicilary', 'salary')", name= "valid_account_types"))
+
+
+# validate transaction type
+class TransactionType(str, Enum):
+    deposite = "deposite"
+    withdrawal = "withdrwal"
+    transfer = "transfer"
+    payment = "payment"
+    credit = "credit"
+    foreign = "foreign"
+
+
+# create Transaction model
+class Transaction(SQLModel, table=True):
+    transaction_id: Optional[int] = Field(default=None, primary_key=True)
+    transaction_type: TransactionType = Field(nullable=False)
+    transaction_date: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), nullable=False) 
+    transaction_amount: Decimal = Field(sa_column_kwargs={"type": "Numeric(14,2)"}, nullable=False)
+    transaction_reference: str = Field(max_length=50, nullable=False)
+    transaction_status: str = Field(max_length=50, nullable=False) 
+    account_id: int = Field(
+                       foreign_key="account.account_id",
+                       sa_column_kwargs={"onupdate": "CASCADE", "ondelete": "NO ACTION"})
+    account:Account = Relationship(back_populates="transactions")
     
+    
+    
+    __table_arg__ = (CheckConstraint("transaction_type IN('deposit', 'withdrawal', 'transfer', 'payment', 'credit', 'foreign')", name="valid_transaction_type"))    
     
